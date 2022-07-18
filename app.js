cf92aa68359810a5d236bb004d582d211b29a8a1
Ethan Salonga <ethansalonga28@gmail.com>
2022-07-18 18:52:11 -0400

    Q3 - Sort by highest to lowest price

--- app.js
+++ app.js
@@ -21,3 +21,16 @@ function sortLowToHigh(numbers) {
 }
 
 console.log(sortLowToHigh([1, 5, 0, 10, 4]))
+
+// Q3. Sort by highest to lowest price
+function sortHighToLow(numbers) {
+  return numbers.sort((a, b) => b.price - a.price)
+}
+
+console.log(
+  sortHighToLow([
+    { id: 1, price: 50 },
+    { id: 2, price: 0 },
+    { id: 3, price: 500 },
+  ])
+)
