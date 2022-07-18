abeb081167bdf92e8288c58d7c4b7a6d8084fb0a
Ethan Salonga <ethansalonga28@gmail.com>
2022-07-18 18:47:42 -0400

    Q2 - Sort by lowest to highest price

--- app.js
+++ app.js
@@ -14,3 +14,10 @@ function showRating(rating) {
 }
 
 console.log(showRating(4.5))
+
+// Q2. Sort by lowest to highest price
+function sortLowToHigh(numbers) {
+  return numbers.sort((a, b) => a - b)
+}
+
+console.log(sortLowToHigh([1, 5, 0, 10, 4]))
